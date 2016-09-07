b1768e3e7184266a50e6fd40ed3a91a8a68daa53
Namida <hayanninja@yandex.com>
2016-09-08 05:26:43 +1200

    Removed commit ID from version number; removed experimental marker.

--- LemStrings.pas
+++ LemStrings.pas
@@ -16,7 +16,7 @@ uses
   function NumericalVersionToStringVersion(Main, Sub, Minor: Integer): String;
 
 const
-  PVersion = '1.47n:E573C5B';
+  PVersion = '1.47n';
 
   // Current version number
   Cur_MainVer = 1;
