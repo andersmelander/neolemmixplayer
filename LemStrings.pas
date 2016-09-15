3ac171aaeeb744ebd7fc36a80b5d961973465588
Namida <hayanninja@yandex.com>
2016-09-15 16:35:26 +1200

    Updated version number.

--- LemStrings.pas
+++ LemStrings.pas
@@ -16,12 +16,12 @@ uses
   function NumericalVersionToStringVersion(Main, Sub, Minor: Integer): String;
 
 const
-  PVersion = '1.47n-B';
+  PVersion = '1.47n-C';
 
   // Current version number
   Cur_MainVer = 1;
   Cur_SubVer = 47;
-  Cur_MinorVer = 2;
+  Cur_MinorVer = 3;
 
   // Do not forget to update consts in TAppController.CheckCompatibility if minimum compatible version has changed!
 
