c9d351198bb86296b909c887b2aa4c58648c7d71
Namida <hayanninja@yandex.com>
2016-09-30 01:35:46 +1300

    Updated commit ID.

--- LemStrings.pas
+++ LemStrings.pas
@@ -16,7 +16,7 @@ uses
   function NumericalVersionToStringVersion(Main, Sub, Minor: Integer): String;
 
 const
-  PVersion = '1.48n:C38045A';
+  PVersion = '1.48n:197BAA1';
 
   // Current version number
   Cur_MainVer = 1;
