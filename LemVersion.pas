018a14963e3529b5517970a27c8f000a3a6faf10
Namida <hayanninja@yandex.com>
2016-12-30 17:49:46 +1300

    Updated versions.

--- LemVersion.pas
+++ LemVersion.pas
@@ -12,7 +12,7 @@ const
   CORE_VERSION = 12;
   FEATURES_VERSION = 12;
   HOTFIX_VERSION = 0;
-  COMMIT_ID = 'e41a39b';  // empty string is handled, and is uppercased when needed so don't need to do manually anymore :D
+  COMMIT_ID = '53be4d8';  // empty string is handled, and is uppercased when needed so don't need to do manually anymore :D
 
 function MakeVersionString(aFormat, aCore, aFeature, aHotfix: Integer): String;
 function MakeVersionID(aFormat, aCore, aFeature, aHotfix: Integer): Int64;
