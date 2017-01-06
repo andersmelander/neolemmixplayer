34cd75d0b80dc64ca90d9f053a00ab03c88fa274
Namida <hayanninja@yandex.com>
2017-01-06 17:10:14 +1300

    Updated commit ID.

--- LemVersion.pas
+++ LemVersion.pas
@@ -12,7 +12,7 @@ const
   CORE_VERSION = 12;
   FEATURES_VERSION = 12;
   HOTFIX_VERSION = 0;
-  COMMIT_ID = '8e38a5f';  // empty string is handled, and is uppercased when needed so don't need to do manually anymore :D
+  COMMIT_ID = '3654989';  // empty string is handled, and is uppercased when needed so don't need to do manually anymore :D
 
 function MakeVersionString(aFormat, aCore, aFeature, aHotfix: Integer): String;
 function MakeVersionID(aFormat, aCore, aFeature, aHotfix: Integer): Int64;
