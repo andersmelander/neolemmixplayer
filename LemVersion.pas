a049d8a95fce8d1e96f2cddcfa4d34fbf121971c
Namida <hayanninja@yandex.com>
2017-04-23 22:55:54 +1200

    Updated hotfix version.

--- LemVersion.pas
+++ LemVersion.pas
@@ -11,7 +11,7 @@ const
   FORMAT_VERSION = 10;
   CORE_VERSION = 13;
   FEATURES_VERSION = 16;
-  HOTFIX_VERSION = 0;
+  HOTFIX_VERSION = 2;
   COMMIT_ID = '63cd48d';  // empty string is handled, and is uppercased when needed so don't need to do manually anymore :D
 
 function MakeVersionString(aFormat, aCore, aFeature, aHotfix: Integer): String;
