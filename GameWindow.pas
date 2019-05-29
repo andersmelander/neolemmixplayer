745ff40dbe885cb366a1f6739a202da4bdfce31f
Namida Verasche <namida.verasche@gmail.com>
2019-05-30 10:33:52 +1200

    TGameWindow.StartReplay2 renamed to just "StartReplay", as StartReplay[1] has not existed for a long time.

--- GameWindow.pas
+++ GameWindow.pas
@@ -104,7 +104,7 @@ type
     procedure AddSaveState;
     procedure CheckAdjustSpawnInterval;
     procedure SetAdjustedGameCursorPoint(BitmapPoint: TPoint);
-    procedure StartReplay2(const aFileName: string);
+    procedure StartReplay(const aFileName: string);
     procedure InitializeCursor;
     procedure CheckShifts(Shift: TShiftState);
     procedure CheckUserHelpers;
@@ -1699,7 +1699,7 @@ begin
   Game.CheckAdjustSpawnInterval;
 end;
 
-procedure TGameWindow.StartReplay2(const aFileName: string);
+procedure TGameWindow.StartReplay(const aFileName: string);
 var
   ext: String;
 
@@ -1808,7 +1808,7 @@ begin
   end;
   if s <> '' then
   begin
-    StartReplay2(s);
+    StartReplay(s);
     exit;
   end;
   CanPlay := OldCanPlay;
