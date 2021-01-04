1f99832e6e59cc916fe0a7e5c2296966501f2703
Namida Verasche <namida.verasche@gmail.com>
2021-01-04 16:13:46 +1300

    Version / switch.

--- LemVersion.pas
+++ LemVersion.pas
@@ -10,7 +10,7 @@ uses
 const
   FORMAT_VERSION = 12;
   CORE_VERSION = 10;
-  FEATURES_VERSION = 1;
+  FEATURES_VERSION = 3;
   HOTFIX_VERSION = 0; // or RC version
 
   STYLE_VERSION = '12.8/'; // For server usage - a new style version should only be used when backwards compatibility breaks.
