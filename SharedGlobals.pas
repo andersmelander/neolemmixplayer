bfcf827e2745f612ef063c9db58c74a63415daea
Willicious <williciousmedia@gmail.com>
2024-11-27 22:33:10 +0000

    Added custom Output() and CustomAssert procedures to SharedGlobals... - Output() writes a custom debug string for debugging purposes - CustomAssert shows a custom dialog and terminates the program (prevents the user having to use Task Manager to close)

--- SharedGlobals.pas
+++ SharedGlobals.pas
@@ -5,7 +5,7 @@ unit SharedGlobals;
 interface
 
 uses
-  {$ifdef logging}Dialogs,{$endif} Classes, SysUtils;
+  Dialogs, Forms, Classes, SysUtils, Windows;
 
 {$ifdef logging}
 var
@@ -14,7 +14,51 @@ var
   procedure DebugMsg(const aString: String);
   procedure Log(const aString: String);
 
+  // For writing to Messages output
+  procedure OutputMarker(aString: String);
+  procedure Output(aString: String) overload;
+  procedure Output(aString: String; i: Integer) overload;
+  procedure Output(aString: String; aBool: Boolean) overload;
+
+  procedure CustomAssert(Condition: Boolean; const aMessage: string);
+
 implementation
+
+procedure CustomAssert(Condition: Boolean; const aMessage: string);
+begin
+  if not Condition then
+  begin
+    ShowMessage(aMessage + sLineBreak + sLineBreak + 'The program will now close');
+    Application.Terminate;
+  end;
+end;
+
+procedure Output(aString: String);
+begin
+  OutputDebugString(PChar(aString));
+end;
+
+procedure Output(aString: String; i: Integer);
+begin
+  OutputDebugString(PChar(aString + ' = ' + i.ToString));
+end;
+
+procedure Output(aString: String; aBool: Boolean);
+var
+  S: String;
+begin
+  if (aBool = True) then S := 'True' else S := 'False';
+  OutputDebugString(PChar(aString + ' = ' + S));
+end;
+
+procedure OutputMarker(aString: String);
+begin
+  if aString = '' then aString := 'Marker';
+
+  OutputDebugString(PChar('////////////////////////////////////////////////////////////////////'));
+  OutputDebugString(PChar('///////////////////////////' + aString + '//////////////////////////'));
+  OutputDebugString(PChar('////////////////////////////////////////////////////////////////////'));
+end;
 
 procedure Log(const aString: String);
 begin
